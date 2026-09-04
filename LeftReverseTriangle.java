dd8d3af5625263082efc58a280657e705a1cccf4
Prem Sagar Thakur <premsagarthakur2080@gmail.com>
2026-09-05 01:49:02 +0530

    day1

---
+++ LeftReverseTriangle.java
@@ -0,0 +1,12 @@
+public class LeftReverseTriangle {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1 ; i<=n ;i++){
+            for (int j = 5; j>=i;j--){
+                System.out.print(n+1-i);
+            }
+            System.out.println();
+        }
+    }
+    
+}
